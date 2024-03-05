326ac1217bd394cd75d9e526c6f8fad3016e57cc
Videk3 <vid.berlocnik2@scv.si>
2024-03-05 15:43:01 +0100

    Added GUI for Statistics form

--- Diplomska/StatisticsForm.Designer.cs
+++ Diplomska/StatisticsForm.Designer.cs
@@ -28,12 +28,297 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.avgDrakesTextBox = new System.Windows.Forms.TextBox();
+            this.avgHeraldTextBox = new System.Windows.Forms.TextBox();
+            this.avgCreepScoreTextBox = new System.Windows.Forms.TextBox();
+            this.avgBaronsTextBox = new System.Windows.Forms.TextBox();
+            this.avgAssistsTextBox = new System.Windows.Forms.TextBox();
+            this.avgDeathsTextBox = new System.Windows.Forms.TextBox();
+            this.avgKillsTextBox = new System.Windows.Forms.TextBox();
+            this.avgVisionScoreTextBox = new System.Windows.Forms.TextBox();
+            this.mostPlayedChampionTextBox = new System.Windows.Forms.TextBox();
+            this.mostPlayedRoleTextBox = new System.Windows.Forms.TextBox();
+            this.winPercentageTextBox = new System.Windows.Forms.TextBox();
+            this.avgMatchLenghtTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Average drakes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Average rift heralds";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Average CS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Average barons";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Average assists";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Average deaths";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 139);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Average kills";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(31, 113);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Average Vision score";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(31, 217);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(111, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Average match lenght";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(31, 247);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(37, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Win %";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(31, 269);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(84, 13);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Most played role";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(31, 295);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(113, 13);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Most played champion";
+            // 
+            // avgDrakesTextBox
+            // 
+            this.avgDrakesTextBox.Location = new System.Drawing.Point(150, 6);
+            this.avgDrakesTextBox.Name = "avgDrakesTextBox";
+            this.avgDrakesTextBox.ReadOnly = true;
+            this.avgDrakesTextBox.Size = new System.Drawing.Size(100, 20);
+            this.avgDrakesTextBox.TabIndex = 12;
+            // 
+            // avgHeraldTextBox
+            // 
+            this.avgHeraldTextBox.Location = new System.Drawing.Point(150, 32);
+            this.avgHeraldTextBox.Name = "avgHeraldTextBox";
+            this.avgHeraldTextBox.ReadOnly = true;
+            this.avgHeraldTextBox.Size = new System.Drawing.Size(100, 20);
+            this.avgHeraldTextBox.TabIndex = 13;
+            // 
+            // avgCreepScoreTextBox
+            // 
+            this.avgCreepScoreTextBox.Location = new System.Drawing.Point(150, 84);
+            this.avgCreepScoreTextBox.Name = "avgCreepScoreTextBox";
+            this.avgCreepScoreTextBox.ReadOnly = true;
+            this.avgCreepScoreTextBox.Size = new System.Drawing.Size(100, 20);
+            this.avgCreepScoreTextBox.TabIndex = 15;
+            // 
+            // avgBaronsTextBox
+            // 
+            this.avgBaronsTextBox.Location = new System.Drawing.Point(150, 58);
+            this.avgBaronsTextBox.Name = "avgBaronsTextBox";
+            this.avgBaronsTextBox.ReadOnly = true;
+            this.avgBaronsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.avgBaronsTextBox.TabIndex = 14;
+            // 
+            // avgAssistsTextBox
+            // 
+            this.avgAssistsTextBox.Location = new System.Drawing.Point(150, 188);
+            this.avgAssistsTextBox.Name = "avgAssistsTextBox";
+            this.avgAssistsTextBox.ReadOnly = true;
+            this.avgAssistsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.avgAssistsTextBox.TabIndex = 19;
+            // 
+            // avgDeathsTextBox
+            // 
+            this.avgDeathsTextBox.Location = new System.Drawing.Point(150, 162);
+            this.avgDeathsTextBox.Name = "avgDeathsTextBox";
+            this.avgDeathsTextBox.ReadOnly = true;
+            this.avgDeathsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.avgDeathsTextBox.TabIndex = 18;
+            // 
+            // avgKillsTextBox
+            // 
+            this.avgKillsTextBox.Location = new System.Drawing.Point(150, 136);
+            this.avgKillsTextBox.Name = "avgKillsTextBox";
+            this.avgKillsTextBox.ReadOnly = true;
+            this.avgKillsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.avgKillsTextBox.TabIndex = 17;
+            // 
+            // avgVisionScoreTextBox
+            // 
+            this.avgVisionScoreTextBox.Location = new System.Drawing.Point(150, 110);
+            this.avgVisionScoreTextBox.Name = "avgVisionScoreTextBox";
+            this.avgVisionScoreTextBox.ReadOnly = true;
+            this.avgVisionScoreTextBox.Size = new System.Drawing.Size(100, 20);
+            this.avgVisionScoreTextBox.TabIndex = 16;
+            // 
+            // mostPlayedChampionTextBox
+            // 
+            this.mostPlayedChampionTextBox.Location = new System.Drawing.Point(150, 292);
+            this.mostPlayedChampionTextBox.Name = "mostPlayedChampionTextBox";
+            this.mostPlayedChampionTextBox.ReadOnly = true;
+            this.mostPlayedChampionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.mostPlayedChampionTextBox.TabIndex = 23;
+            // 
+            // mostPlayedRoleTextBox
+            // 
+            this.mostPlayedRoleTextBox.Location = new System.Drawing.Point(150, 266);
+            this.mostPlayedRoleTextBox.Name = "mostPlayedRoleTextBox";
+            this.mostPlayedRoleTextBox.ReadOnly = true;
+            this.mostPlayedRoleTextBox.Size = new System.Drawing.Size(100, 20);
+            this.mostPlayedRoleTextBox.TabIndex = 22;
+            // 
+            // winPercentageTextBox
+            // 
+            this.winPercentageTextBox.Location = new System.Drawing.Point(150, 240);
+            this.winPercentageTextBox.Name = "winPercentageTextBox";
+            this.winPercentageTextBox.ReadOnly = true;
+            this.winPercentageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.winPercentageTextBox.TabIndex = 21;
+            // 
+            // avgMatchLenghtTextBox
+            // 
+            this.avgMatchLenghtTextBox.Location = new System.Drawing.Point(150, 214);
+            this.avgMatchLenghtTextBox.Name = "avgMatchLenghtTextBox";
+            this.avgMatchLenghtTextBox.ReadOnly = true;
+            this.avgMatchLenghtTextBox.Size = new System.Drawing.Size(100, 20);
+            this.avgMatchLenghtTextBox.TabIndex = 20;
+            // 
+            // StatisticsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mostPlayedChampionTextBox);
+            this.Controls.Add(this.mostPlayedRoleTextBox);
+            this.Controls.Add(this.winPercentageTextBox);
+            this.Controls.Add(this.avgMatchLenghtTextBox);
+            this.Controls.Add(this.avgAssistsTextBox);
+            this.Controls.Add(this.avgDeathsTextBox);
+            this.Controls.Add(this.avgKillsTextBox);
+            this.Controls.Add(this.avgVisionScoreTextBox);
+            this.Controls.Add(this.avgCreepScoreTextBox);
+            this.Controls.Add(this.avgBaronsTextBox);
+            this.Controls.Add(this.avgHeraldTextBox);
+            this.Controls.Add(this.avgDrakesTextBox);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "StatisticsForm";
             this.Text = "StatisticsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox avgDrakesTextBox;
+        private System.Windows.Forms.TextBox avgHeraldTextBox;
+        private System.Windows.Forms.TextBox avgCreepScoreTextBox;
+        private System.Windows.Forms.TextBox avgBaronsTextBox;
+        private System.Windows.Forms.TextBox avgAssistsTextBox;
+        private System.Windows.Forms.TextBox avgDeathsTextBox;
+        private System.Windows.Forms.TextBox avgKillsTextBox;
+        private System.Windows.Forms.TextBox avgVisionScoreTextBox;
+        private System.Windows.Forms.TextBox mostPlayedChampionTextBox;
+        private System.Windows.Forms.TextBox mostPlayedRoleTextBox;
+        private System.Windows.Forms.TextBox winPercentageTextBox;
+        private System.Windows.Forms.TextBox avgMatchLenghtTextBox;
     }
 }
